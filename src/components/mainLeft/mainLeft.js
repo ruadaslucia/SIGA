1360113aaae679f892a3775f6e7853468e1dea3b
lucia <ruadaslucia@gmail.com>
2022-06-17 11:51:38 +0200

    sidebar

--- src/components/mainLeft/mainLeft.js
+++ src/components/mainLeft/mainLeft.js
@@ -1,10 +1,22 @@
 import './mainLeft.scss';
+import { ProSidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
+import 'react-pro-sidebar/dist/css/styles.css';
 
 function MainLeft() {
   return (
-    <main className="mainLeft">
+    <div className="mainLeft">
       <h2 className="mainTitle">mainLeft</h2>
-    </main>
+      <ProSidebar>
+        <Menu iconShape="square">
+          <MenuItem icon="">Menu</MenuItem>
+          <SubMenu title="Cursos" icon="">
+            <MenuItem>2021/2022</MenuItem>
+            <MenuItem>2022/2023</MenuItem>
+          </SubMenu>
+        </Menu>
+      </ProSidebar>
+      ;
+    </div>
   );
 }
 
--- src/components/mainTop/mainTop.js
+++ src/components/mainTop/mainTop.js
@@ -4,7 +4,7 @@ import UserImg from '../../images/studentIcon.png';
 function MainTop() {
   return (
     <div className="mainTop">
-      <h2 className="mainTop__mainTitle">esto es el título</h2>
+      <h2 className="mainTop__mainTitle">mainTop</h2>
       <img className="mainTop__userImage" src={UserImg} alt="logo" />
     </div>
   );
