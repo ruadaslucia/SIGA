6e9620992291d4420938844ddf50d034ebc447bc
lucia <ruadaslucia@gmail.com>
2022-06-21 13:02:31 +0200

    sidebar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import './App.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 import Main from './components/main/main';
 import Landing from './components/landing/landing';
 import { Route, Routes } from 'react-router-dom';
--- src/components/main/main.js
+++ src/components/main/main.js
@@ -1,12 +1,10 @@
 import './main.scss';
-import MainTop from '../mainTop/mainTop';
-import SideBar from '../sideBar/sideBar';
+import SideBar from '../sideBar/mainLeft';
 import MainRight from '../mainRight/mainRight';
 
 function Main() {
   return (
     <main className="main">
-      <MainTop />
       <SideBar />
       <MainRight />
       <div className="titleContainer"></div>
