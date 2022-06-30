7e255760294be35795c3dccf1a1c9109c41142df
Lucia <ruadaslucia@gmail.com>
2022-06-30 13:47:45 +0200

    refactor project

---
+++ web/src/components/profile/Profile.js
@@ -0,0 +1,10 @@
+import '../profile/profile.scss';
+function Profile() {
+  return (
+    <div className="profile">
+      <p>Mi perfil</p>
+    </div>
+  );
+}
+
+export default Profile;
---
+++ web/src/components/students/Students.js
@@ -0,0 +1,10 @@
+import '../students/students.scss';
+function Students() {
+  return (
+    <div className="students">
+      <p>Mis alumnos</p>
+    </div>
+  );
+}
+
+export default Students;
