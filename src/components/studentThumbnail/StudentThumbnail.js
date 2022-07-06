8995e7a8b08a68befea1843c4708a6ccfcbd3bda
Lucia <ruadaslucia@gmail.com>
2022-07-06 19:38:28 +0200

    buttons to icons

--- src/components/studentThumbnail/StudentThumbnail.js
+++ src/components/studentThumbnail/StudentThumbnail.js
@@ -7,10 +7,17 @@ import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import './studentThumbnail.scss';
 import studentAvatar from '../../images/avatarAlumno.jpg';
-import DeleteButton from './deleteButton/DeleteButton';
-import StudentDetail from './studentDetail/StudentDetail';
+
+import CancelIcon from '@mui/icons-material/Cancel';
+import PersonRoundedIcon from '@mui/icons-material/PersonRounded';
 
 function StudentThumbnail() {
+  function handleStudentDetail() {
+    console.log('Este botón abre el detalle del alumno');
+  }
+  function handleDeleteButton() {
+    console.log('Este botón elimina este alumno');
+  }
   return (
     <div>
       <Card className="card">
@@ -18,17 +25,22 @@ function StudentThumbnail() {
         <CardHeader className="cardStudentName" title="Full Name" />
 
         <CardContent>
-          <Typography
-            className="cardText"
-            variant="body2"
-            color="text.secondary"
-          >
+          <Typography variant="body2" color="text.secondary">
             Text with another information of the student.
           </Typography>
         </CardContent>
         <div className="cardLink">
-          <StudentDetail />
-          <DeleteButton />
+          <button
+            className="studenThumbNailButton"
+            onClick={handleStudentDetail}
+            variant="contained"
+            endIcon={<CancelIcon />}
+          ></button>
+          <button
+            className="studenThumbNailButton"
+            onClick={handleDeleteButton}
+            endIcon={<PersonRoundedIcon />}
+          ></button>
         </div>
       </Card>
     </div>
